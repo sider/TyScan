393bef14e56ab135b283c6fbb61a77428b764349
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-01-23 07:34:04 +0900

    build → dist

--- build/pattern/util.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const undefined = 1;
-//# sourceMappingURL=util.js.map
--- build/pattern/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ts = require("typescript");
-exports.isExpressionNode = ts.isExpressionNode;
-//# sourceMappingURL=utils.js.map
