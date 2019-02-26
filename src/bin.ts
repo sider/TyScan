87251b84712981428fdb2b6a7a136d5500cc2481
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-02-26 18:15:24 +0900

    Remove unnecessary imports

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,3 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as commander from 'commander';
 import * as cli from './cli';
 import { configureCompilerOptions } from './compiler';
