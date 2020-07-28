86904e657296c75e024d69c4d19c3cdfdac30b8c
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-29 03:09:01 +0900

    Change `scan` output default format (breaking) (#147)

--- src/cli/subcommand/scanCommand.ts
+++ src/cli/subcommand/scanCommand.ts
@@ -41,10 +41,10 @@ export class ScanCommand extends Command {
                 },
               });
             } else {
-              const loc = `${result.path}#L${start.line + 1}C${start.character + 1}`;
-              const msg = `${rule.message} (${rule.id})`;
-              const txt = `${loc}\t${node.getText()}\t${msg}`;
-              this.stdout(`${txt}`);
+              const line = start.line + 1;
+              const column = start.character + 1;
+              const message = (rule.message.match(/[^\r\n]+/) || [])[0]; // only the first line
+              this.stdout(`${result.path}:${line}:${column}: ${message} (${rule.id})`);
             }
           }
         }
