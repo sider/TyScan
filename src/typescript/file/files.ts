111aa5d56a3de5e5257c3c086791bf99f1c2e581
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-29 02:41:21 +0900

    Show message if no files to scan
    E.g.
    
    ```console
    $ tyscan.js scan src/foo/
    No files to scan
    ```

--- src/typescript/file/files.ts
+++ src/typescript/file/files.ts
@@ -3,24 +3,38 @@ import * as fs from 'fs';
 import { File } from './file';
 import { RealFile } from './realFile';
 
-export class Files extends Array<File> {
-
+export class Files {
   static load(paths: string[]) {
-    const normalizedPaths = paths.map(p => p.replace(/\/$/, ''));
-    const validPaths = normalizedPaths.filter(p => fs.existsSync(p));
+    const normalizedPaths = paths.map((p) => p.replace(/\/$/, ''));
+    const validPaths = normalizedPaths.filter((p) => fs.existsSync(p));
 
-    const filePaths = validPaths.map((p) => {
-      if (fs.statSync(p).isDirectory()) {
-        return fg.sync([`${p}/**/*.{ts,tsx}`]).map(e => e.toString());
-      }
-      return [p];
-    }).reduce((acc, paths) => acc.concat(paths), []);
+    const filePaths = validPaths
+      .map((p) => {
+        if (fs.statSync(p).isDirectory()) {
+          return fg.sync([`${p}/**/*.{ts,tsx}`]).map((e) => e.toString());
+        }
+        return [p];
+      })
+      .reduce((acc, paths) => acc.concat(paths), []);
 
-    const files = filePaths.map(p => new RealFile(p));
-    return new Files(...files);
+    return new Files(filePaths.map((p) => new RealFile(p)));
   }
 
-  findByPath(path: string) {
-    return this.find(f => f.path === path);
+  private list: readonly File[];
+
+  constructor(list: File[]) {
+    this.list = Object.freeze(list);
+  }
+
+  findByPath(path: string): File | undefined {
+    return this.list.find((f) => f.path === path);
+  }
+
+  map<T>(func: (file: File) => T): T[] {
+    return this.list.map(func);
+  }
+
+  isEmpty(): boolean {
+    return this.list.length === 0;
   }
 }
