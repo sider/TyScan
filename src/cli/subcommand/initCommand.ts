d453cda520bd0fc5f3d0a82bc48c4f4aa64f4f36
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-29 01:01:29 +0900

    Avoid importing `constants` module
    Use `fs.constants` instead of `import ... from 'constants'`.
    
    - https://nodejs.org/api/fs.html#fs_fs_copyfilesync_src_dest_mode
    - https://nodejs.org/api/fs.html#fs_fs_constants

--- src/cli/subcommand/initCommand.ts
+++ src/cli/subcommand/initCommand.ts
@@ -1,14 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { COPYFILE_EXCL } from 'constants';
 import { Command } from './command';
 
 export class InitCommand extends Command {
-
   private static samplePath = path.resolve(__dirname, '../../../sample/tyscan.yml');
 
   run() {
-    fs.copyFileSync(InitCommand.samplePath, 'tyscan.yml', COPYFILE_EXCL);
+    fs.copyFileSync(InitCommand.samplePath, 'tyscan.yml', fs.constants.COPYFILE_EXCL);
     return 0;
   }
 }
